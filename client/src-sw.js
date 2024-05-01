08847ccd321f153bb4c98475597dd38f66edd823
SamuelFullerCA <samuelfullerca@gmail.com>
2024-05-01 10:18:52 -0700

    updates

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,24 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// Implemented asset caching
+
+const assetCache = new CacheFirst({
+  cacheName: 'asset-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ]
+})
+
+registerRoute(({ request }) =>
+  // Styles
+  request.destination === 'style' ||
+  //Javascript
+  request.destination === 'script',
+  assetCache
+);
